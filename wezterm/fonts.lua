9d74ddc529b234c45489b478692abb62151838d8
mikah13 <anhminhhoang13@gmail.com>
2024-01-09 02:36:00 -0800

    update

--- wezterm/fonts.lua
+++ wezterm/fonts.lua
@@ -1,13 +1,12 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local C = {}
 
 function C.apply(config)
-  config.font = wezterm.font_with_fallback {
-    { family = "JetBrains Mono", weight = "Medium" , harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },},
-    
-  }
-  config.font_size = 12
-  config.cell_width = 0.9
+	config.font = wezterm.font_with_fallback({
+		{ family = "JetBrains Mono", weight = "Medium", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } },
+	})
+	config.font_size = 12
+	config.cell_width = 0.9
 end
 
 return C
